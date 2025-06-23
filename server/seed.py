ee38f258fad1c2f8113ba76a65c78bbabd2b3a41
Mohamed-Abass2287 <aminabass987@gmail.com>
2025-06-23 20:01:00 +0300

    completed challange

--- server/seed.py
+++ server/seed.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from random import randint, choice as rc
-
 from faker import Faker
 
 from app import app
@@ -10,54 +9,43 @@ from models import db, Recipe, User
 fake = Faker()
 
 with app.app_context():
-
     print("Deleting all records...")
     Recipe.query.delete()
     User.query.delete()
 
-    fake = Faker()
-
     print("Creating users...")
-
-    # make sure users have unique usernames
     users = []
-    usernames = []
 
-    for i in range(20):
-        
-        username = fake.first_name()
-        while username in usernames:
-            username = fake.first_name()
-        usernames.append(username)
-
+    for _ in range(20):
+        username = fake.unique.first_name()
         user = User(
             username=username,
             bio=fake.paragraph(nb_sentences=3),
-            image_url=fake.url(),
+            image_url="https://cdn.iconscout.com/icon/free/png-256/avatar-372-456324-screenshot_4.jpg"
         )
-
-        user.password_hash = user.username + 'password'
-
+        user.password_hash = f"{username}123"
         users.append(user)
 
     db.session.add_all(users)
+    db.session.commit()
 
     print("Creating recipes...")
     recipes = []
-    for i in range(100):
-        instructions = fake.paragraph(nb_sentences=8)
-        
+
+    for _ in range(100):
+        user = rc(users)
+        instructions = fake.paragraph(nb_sentences=10)
+        if len(instructions) < 50:
+            instructions += " Additional steps added to meet the required 50 characters."
+
         recipe = Recipe(
             title=fake.sentence(),
             instructions=instructions,
-            minutes_to_complete=randint(15,90),
+            minutes_to_complete=randint(15, 90),
+            user_id=user.id
         )
-
-        recipe.user = rc(users)
-
         recipes.append(recipe)
 
     db.session.add_all(recipes)
-    
     db.session.commit()
-    print("Complete.")
+    print("Seeding complete.")
